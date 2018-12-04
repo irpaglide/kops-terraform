5e718af8ae2dec9d5ba1529668e6a99057225b79
juan.garcia <juan.garcia@waterworks.com>
2018-12-04 12:50:35 -0300

    validate

--- kops-validate.sh
+++ kops-validate.sh
@@ -2,5 +2,13 @@ export PATH=$PATH:/usr/local/bin
 
 ZONES=$(terraform output availability_zones|sed s/,//g | paste -s -d,)
 NAME=$(terraform output name_cluster)
+STATE=$(terraform output state_store)
+RET=1
+until [ "$RET" == "0" ] ; do
 kops validate cluster \
-    --name ${NAME} --state $(terraform output state_store) 
+    --name ${NAME} --state ${STATE} 2>&1  > /dev/null
+echo -n "."
+done
+echo
+kops validate cluster \
+    --name ${NAME} --state ${STATE}
